c44f3c9c65f979114217307bedbe9093bde82706
kdhttps <kdhttps@gmail.com>
2019-09-01 13:44:29 +0530

    Added test case

---
+++ src/auth/auth.test.js
@@ -0,0 +1,12 @@
+/**
+ * Created by KD
+ */
+const expect  = require('chai').expect;
+const { handleCatch } = require('../utils/common');
+
+describe('Test Common functions', function() {
+  it('Test handleCatch', function(done) {
+    expect(true).to.equal(true);
+    done();
+  });
+});
